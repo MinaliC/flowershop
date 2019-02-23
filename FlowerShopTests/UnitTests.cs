cbac90a38872914550cd4496299a72ef7ad819b1
Minali <minali.chouhan@gmail.com>
2019-02-23 14:31:39 +0200

    Made changes to unit tests

--- FlowerShopTests/UnitTests.cs
+++ FlowerShopTests/UnitTests.cs
@@ -1,4 +1,7 @@
+using FlowerShop;
+using NSubstitute;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -12,6 +15,21 @@ namespace Tests
         [Test]
         public void Test1()
         {
+            //Arrange
+            IClient c = Substitute.For<IClient>();
+            IOrderDAO a = Substitute.For<IOrderDAO>();
+            List<Flower> f = new List<Flower>();
+            IOrder o = new Order(a, c, false);
+
+            //Act
+            o.Deliver();
+
+            //Assert
+            a.SetDelivered(Arg.Any<IOrder>());
+
+
+
+
             Assert.Pass();
         }
     }
