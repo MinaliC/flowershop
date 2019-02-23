acbd7663497f667f0eca89066c1b69557c279b86
Minali <minali.chouhan@gmail.com>
2019-02-23 14:39:03 +0200

    Second commit

--- FlowerShop/Order.cs
+++ FlowerShop/Order.cs
@@ -7,7 +7,7 @@ namespace FlowerShop
     public class Order : IOrder, IIdentified
     {
         private List<Flower> flowers;
-        private bool isDelivered = false;
+        private bool isDelivered = true;
         public int Id { get; }
 
         // should apply a 20% mark-up to each flower.
